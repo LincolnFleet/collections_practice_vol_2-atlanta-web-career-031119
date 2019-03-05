e7a719253faaaf0e4da3c647f11d2a67a54d12e6
Travis Martin <travisemerymartin@gmail.com>
2019-03-05 17:34:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,17 +1,13 @@
 # your code goes here
 def begins_with_r(array)
-  if array.length>0
-    i=0
-    while i<array.length
-      if array[i][0] == 'r'
-        i+=1
-      else
-        return false
-      end
-      return true
+  i=0
+  while i<array.length
+    if array[i][0] == 'r'
+      i+=1
+    else
+      return false
     end
-  else
-    return false
+    return true
   end
 end
 
