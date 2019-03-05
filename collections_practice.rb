7811c7226617710abfacb872f2ee4a9e69b60a4e
Travis Martin <travisemerymartin@gmail.com>
2019-03-05 17:31:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,7 @@ def begins_with_r(array)
   if array.length>0
     i=0
     while i<array.length
-      if array[i][0] == 'r'
+      if array[i].first == 'r'
         i+=1
       else
         return false
